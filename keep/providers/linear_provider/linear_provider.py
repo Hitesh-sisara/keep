b889a1aed26e615a4fcef656a750151f8c3d8ddb
Hitesh Sisara <hitsara99@gmail.com>
2023-10-01 15:16:21 +0530

    Update linear_provider.py
    Signed-off-by: Hitesh Sisara <hitsara99@gmail.com>

--- keep/providers/linear_provider/linear_provider.py
+++ keep/providers/linear_provider/linear_provider.py
@@ -7,6 +7,7 @@ from keep.exceptions.provider_exception import ProviderException
 from keep.providers.base.base_provider import BaseProvider
 from keep.providers.models.provider_config import ProviderConfig
 
+
 @pydantic.dataclasses.dataclass
 class LinearProviderAuthConfig:
     """Linear authentication configuration."""
@@ -18,63 +19,116 @@ class LinearProviderAuthConfig:
             "sensitive": True,
         }
     )
-    project_id: str = dataclasses.field(
+    team_id: str = dataclasses.field(
         metadata={
             "required": True,
-            "description": "Linear Project ID",
+            "description": "Linear Team ID",
         }
     )
 
+
 class LinearProvider(BaseProvider):
-    def __init__(
-        self, context_manager: ContextManager, provider_id: str, config: ProviderConfig
-    ):
+    BASE_URL = "https://api.linear.app/graphql"
+
+    def __init__(self, context_manager: ContextManager, provider_id: str, config: ProviderConfig):
         super().__init__(context_manager, provider_id, config)
+        self.validate_config()
 
     def validate_config(self):
-        self.authentication_config = LinearProviderAuthConfig(
-            **self.config.authentication
-        )
+        self.authentication_config = LinearProviderAuthConfig(**self.config.authentication)
 
     def dispose(self):
-        """
-        No need to dispose of anything, so just do nothing.
-        """
         pass
 
-    def _query(self, **kwargs: dict):
-        """
-        API for fetching Linear data.
-
-        Args:
-            kwargs (dict): Additional parameters for the request.
-        """
-        self.logger.debug("Fetching data from Linear")
-
-        linear_api_token = self.authentication_config.api_token
-        project_id = self.authentication_config.project_id
-
-        # Construct the request URL and headers based on the Linear API documentation
-        request_url = f"https://api.linear.app/v1/projects/{project_id}"
+    def _make_request(self, query: str, variables: dict = None):
+        """Utility function to make GraphQL requests to Linear API."""
         headers = {
-            "Authorization": f"Bearer {linear_api_token}",
+            "Authorization": f"Bearer {self.authentication_config.api_token}",
             "Content-Type": "application/json",
         }
 
-        response = requests.get(request_url, headers=headers)
+        response = requests.post(self.BASE_URL, headers=headers, json={"query": query, "variables": variables})
 
         if not response.ok:
             raise ProviderException(
-                f"{self.__class__.__name__} failed to fetch data from Linear: {response.text}"
+                f"{self.__class__.__name__} failed with response: {response.text}"
             )
 
-        self.logger.debug("Fetched data from Linear")
+        return response.json()
 
-        linear_data = response.json()
-        return {"linear_data": linear_data}
+    def fetch_issues(self):
+        query = """
+        query Team {
+          team(id: $teamId) {
+            issues {
+              nodes {
+                id
+                title
+                description
+              }
+            }
+          }
+        }
+        """
+        variables = {"teamId": self.authentication_config.team_id}
+        return self._make_request(query, variables)
+
+    def create_issue(self, title: str, description: str):
+        query = """
+        mutation IssueCreate($input: IssueCreateInput!) {
+          issueCreate(input: $input) {
+            success
+            issue {
+              id
+              title
+            }
+          }
+        }
+        """
+        variables = {
+            "input": {
+                "title": title,
+                "description": description,
+                "teamId": self.authentication_config.team_id
+            }
+        }
+        return self._make_request(query, variables)
+
+    def edit_issue(self, issue_id: str, title: str = None, description: str = None):
+        query = """
+        mutation IssueUpdate($id: String!, $input: IssueUpdateInput!) {
+          issueUpdate(id: $id, input: $input) {
+            success
+            issue {
+              id
+              title
+              description
+            }
+          }
+        }
+        """
+        input_data = {}
+        if title:
+            input_data["title"] = title
+        if description:
+            input_data["description"] = description
+
+        variables = {"id": issue_id, "input": input_data}
+        return self._make_request(query, variables)
+
+    def delete_issue(self, issue_id: str):
+        query = """
+        mutation IssueDelete($id: String!) {
+          issueDelete(id: $id) {
+            success
+          }
+        }
+        """
+        variables = {"id": issue_id}
+        return self._make_request(query, variables)
+
 
 if __name__ == "__main__":
-    # Output debug messages
     import logging
 
     logging.basicConfig(level=logging.DEBUG, handlers=[logging.StreamHandler()])
@@ -82,19 +136,18 @@ if __name__ == "__main__":
         tenant_id="singletenant",
         workflow_id="test",
     )
-    # Load environment variables
+
     import os
 
     linear_api_token = os.environ.get("LINEAR_API_TOKEN")
-    linear_project_id = os.environ.get("LINEAR_PROJECT_ID")
+    linear_team_id = os.environ.get("LINEAR_TEAM_ID")
 
-    # Initialize the provider and provider config
     config = ProviderConfig(
         description="Linear Input Provider",
         authentication={
             "api_token": linear_api_token,
-            "project_id": linear_project_id,
+            "team_id": linear_team_id,
         },
     )
     provider = LinearProvider(context_manager, provider_id="linear", config=config)
-    provider.query()  
+    provider.fetch_issues()
